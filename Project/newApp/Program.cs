d4edd78bbe0792ac8020c55a6fcaadc054d52c0e
vutrunghau12122002 <haut39708@gmail.com>
2023-09-08 00:04:46 +0700

    commit note

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Cac kieu du lieu trong c# :");
+        int a = 696969;
+        Console.WriteLine("Kieu du lieu int: " + a);
+        long b = 99999;
+        Console.WriteLine("Kieu du lieu long:" + b);
+        float c = -600000;
+        Console.WriteLine("Kieu du lieu float: " + c);
+        double d = -321321321;
+        Console.WriteLine("Kieu du lieu double: " + d);
+        decimal e = -2121;
+        Console.WriteLine("Kieu du lieu decimal: " + e);
+        bool f = true;
+        Console.WriteLine("Kieu du lieu bool:" + f);
+        char g = 'h';
+        Console.WriteLine("Kieu du lieu char: " + g);
+        string h = "Dao Trong Hieu 2021050258";
+        Console.WriteLine("Kieu du lieu string: " + h);
+    }
+}
