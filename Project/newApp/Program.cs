4e0c09f415fe502f7d26be542c77af993a753cc0
vutrunghau12122002 <haut39708@gmail.com>
2023-09-08 00:08:16 +0700

    baitapbienso

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -3,21 +3,21 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Cac kieu du lieu trong c# :");
-        int a = 696969;
+        int a = 69;
         Console.WriteLine("Kieu du lieu int: " + a);
-        long b = 99999;
+        long b = 123;
         Console.WriteLine("Kieu du lieu long:" + b);
-        float c = -600000;
+        float c = -300;
         Console.WriteLine("Kieu du lieu float: " + c);
-        double d = -321321321;
+        double d = -2002;
         Console.WriteLine("Kieu du lieu double: " + d);
-        decimal e = -2121;
+        decimal e = -2002;
         Console.WriteLine("Kieu du lieu decimal: " + e);
         bool f = true;
         Console.WriteLine("Kieu du lieu bool:" + f);
-        char g = 'h';
+        char g = 't';
         Console.WriteLine("Kieu du lieu char: " + g);
-        string h = "Dao Trong Hieu 2021050258";
-        Console.WriteLine("Kieu du lieu string: " + h);
+        string h = "Vu Trung Hau-2021050246";
+        Console.WriteLine("Kieu du lieu strting: " + h);
     }
 }
