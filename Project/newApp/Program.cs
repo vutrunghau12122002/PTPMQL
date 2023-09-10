43d90ba4924eb0ad1884ebcb7e1ee33f0d660dee
vutrunghau12122002 <haut39708@gmail.com>
2023-09-10 19:29:18 +0700

    BTgangiatri

--- Project/newApp/Program.cs
+++ Project/newApp/Program.cs
@@ -1,23 +1,9 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Cac kieu du lieu trong c# :");
-        int a = 69;
-        Console.WriteLine("Kieu du lieu int: " + a);
-        long b = 123;
-        Console.WriteLine("Kieu du lieu long:" + b);
-        float c = -300;
-        Console.WriteLine("Kieu du lieu float: " + c);
-        double d = -2002;
-        Console.WriteLine("Kieu du lieu double: " + d);
-        decimal e = -2002;
-        Console.WriteLine("Kieu du lieu decimal: " + e);
-        bool f = true;
-        Console.WriteLine("Kieu du lieu bool:" + f);
-        char g = 't';
-        Console.WriteLine("Kieu du lieu char: " + g);
-        string h = "Vu Trung Hau-2021050246";
-        Console.WriteLine("Kieu du lieu strting: " + h);
-    }
+private static void Main (string[] args)
+{
+string hoTen = " Vu Trung Hau";
+int msv = 2021050246;
+Console.WriteLine("Sinh vien {0} - {1}",hoTen,msv);
+}
 }
